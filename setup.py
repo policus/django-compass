df9d892c2b31c7a56f949db66be29c47fc04f069
Alex Cabrera <alex@policus.com>
2010-11-08 20:33:20 -0500

    Got tired of trying elegant solutions for package_dataata, hardcoding everything

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
 from distutils.core import setup
 import os
-import logging
-# Compile the list of packages available, because disutils doesn't have an
-# easy way to do this
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
 
-# Snippet from http://django-registration.googlecode.com/svn/trunk/setup.py
-for dirpath, dirnames, filenames in os.walk('compass'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath # Strip "registration/" or "registration\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-            
 setup(
     name='django-compass',
     version='0.1',
@@ -31,8 +9,22 @@ setup(
     author='Policus',
     author_email='Alex Cabrera <alex@policus.com>',
     url='https://github.com/policus/django-compass',
-    packages=packages,
-    data_files=data_files,
+    packages=['compass',],
+    package_data={'compass':[
+        'static/css/sass/_compass.sass',
+        'static/css/sass/compass/*.sass',
+        'static/css/sass/compass/css3/*.sass',
+        'static/css/sass/compass/reset/*.sass',
+        'static/css/sass/compass/utilities/*.sass',
+        'static/css/sass/compass/utilities/general/*.sass',
+        'static/css/sass/compass/utilities/links/*.sass',
+        'static/css/sass/compass/utilities/lists/*.sass',
+        'static/css/sass/compass/utilities/sprites/*.sass',
+        'static/css/sass/compass/utilities/tables/*.sass',
+        'static/css/sass/compass/utilities/text/*.sass',
+        'static/css/sass/frameworks/_susy.sass',
+        'static/css/sass/frameworks/susy/*.sass',
+    ]},
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
